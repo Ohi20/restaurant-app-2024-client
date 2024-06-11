c992057739f62bc072a580914e3c51598414cb29
Ohi20 <ashrafohi20@gmail.com>
2024-06-11 07:30:20 +0600

    homepage-setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
+import PopularMenu from "./popularMenu/PopularMenu";
+import Featured from "./Featured/Featured";
+import Testimonials from "./Testimonials/Testimonials";
+
+function Home() {
+  return (
+    <div>
+      <Banner></Banner>
+      <Category></Category>
+      <PopularMenu></PopularMenu>
+      <Featured></Featured>
+      <Testimonials></Testimonials>
+    </div>
+  );
+}
+
+export default Home;
